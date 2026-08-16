c6860c18d566afadb7361a406b58357729314329
Swajal-Indorkar <swajal41@gmail.com>
2026-08-16 12:47:54 +0530

    fix: add cache buster to static assets

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 import sys
 from server import run
 
+import os
+
 if __name__ == "__main__":
-    port = int(sys.argv[1]) if len(sys.argv) > 1 else 8080
+    env_port = os.environ.get("PORT")
+    port = int(env_port) if env_port else (int(sys.argv[1]) if len(sys.argv) > 1 else 8080)
     run(port)
